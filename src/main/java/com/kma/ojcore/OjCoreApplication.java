035832e8397fcb1f32790cf360b1084e3ce1868a
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-12-10 22:35:22 +0700

    Initial commit

---
+++ src/main/java/com/kma/ojcore/OjCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kma.ojcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kma/ojcore/OjCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kma.ojcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
