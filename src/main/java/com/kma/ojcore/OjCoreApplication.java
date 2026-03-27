846980afe412c4dd2121d88d278c80db21e698e3
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2026-03-27 15:38:55 +0700

    fix(time-zone): set default time zone UTC

--- src/main/java/com/kma/ojcore/OjCoreApplication.java
+++ src/main/java/com/kma/ojcore/OjCoreApplication.java
@@ -1,15 +1,24 @@
 package com.kma.ojcore;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class OjCoreApplication {
+
+    @PostConstruct
+    public void init() {
+        // Thiết lập múi giờ mặc định cho toàn bộ ứng dụng Backend.
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         // Load .env từ thư mục gốc
