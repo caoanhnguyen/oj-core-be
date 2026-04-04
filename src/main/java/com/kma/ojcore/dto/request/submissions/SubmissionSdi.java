4d334e58a03364fdcbe95a4723880ac9b631984d
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2026-04-04 14:04:20 +0700

    feat(submissions): implements valid max size for source code is 64KB

--- src/main/java/com/kma/ojcore/dto/request/submissions/SubmissionSdi.java
+++ src/main/java/com/kma/ojcore/dto/request/submissions/SubmissionSdi.java
@@ -2,6 +2,7 @@ package com.kma.ojcore.dto.request.submissions;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -19,6 +20,7 @@ public class SubmissionSdi {
     String languageKey;
 
     @NotBlank(message = "Source code không được để trống")
+    @Size(max = 65536, message = "Source code is too large! Maximum limit is 64KB.")
     String sourceCode;
 
     UUID contestId;
